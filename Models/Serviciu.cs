d0d5f350cadfebe60fa44732440b50b05331db8f
uSeR <uSeR@DESKTOP-J2OBVSE>
2023-01-05 18:22:25 +0200

    Proiect

---
+++ Models/Serviciu.cs
@@ -0,0 +1,10 @@
+﻿namespace Proiect.Models
+{
+    public class Serviciu
+    {
+        public int ID { get; set; }
+        public string Tip { get; set; }
+        public string Angajat { get; set; }
+        public decimal Pret { get; set; }
+    }
+}
